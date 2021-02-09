ff2e45a3f69372e44c322f605ae0cc8c99c9dcf4
Julia Ronzina <julia.bv@gmail.com>
2021-02-09 22:53:18 +0300

    commit Test

---
+++ src/test/java/tests/PrcticeFormTest.java
@@ -0,0 +1,101 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.hidden;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class PrcticeFormTest {
+    String name = "Juk";
+    String lastName = "Jukov";
+    String email = "juk@jukov.com";
+    String phoneNumber = "3345647891";
+    String monthOfBirth = "April";
+    String yearOfBirth = "1999";
+    String dayOfBirth = "09";
+    String subject = "Chemistry";
+    String gender = "Male";
+    String hobby = "Reading";
+    String currentAddress = "HaryanaKarnal 3 Current Address";
+    String state = "Haryana";
+    String city = "Karnal";
+
+    String filePath="./src/test/resources/pict.jpg";
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized=true;
+    }
+
+    @Test
+    void fillPracticeFormTest() {
+        //arrange
+        open("https://demoqa.com/automation-practice-form");
+        SelenideElement mainHeader = $(".main-header");
+        mainHeader.shouldHave(text("Practice Form"));
+
+        //act
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(phoneNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--0"+dayOfBirth).click();
+        $("#subjectsInput").setValue("c");
+        $(byText(subject)).click();
+        $(byText(hobby)).click();
+        $("#uploadPicture").uploadFile(new File(filePath));
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $(byText(state)).click();
+        $("#city").click();
+        $(byText(city)).click();
+        $("#submit").click();
+
+        //assert
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $$("tbody tr").filterBy(text("Student name")).shouldHave(texts(name + " " + lastName));
+        $$("tbody tr").filterBy(text("Student Email")).shouldHave(texts(email));
+        $$("tbody tr").filterBy(text("Gender")).shouldHave(texts(gender));
+        $$("tbody tr").filterBy(text("Mobile")).shouldHave(texts(phoneNumber));
+        $$("tbody tr").filterBy(text("Date of Birth")).shouldHave(texts(dayOfBirth+" "+monthOfBirth+" "+yearOfBirth));
+        $$("tbody tr").filterBy(text("Subjects")).shouldHave(texts(subject));
+        $$("tbody tr").filterBy(text("Hobbies")).shouldHave(texts(hobby));
+        $$("tbody tr").filterBy(text("Picture")).shouldHave(texts("pict.jpg"));
+        $$("tbody tr").filterBy(text("Address")).shouldHave(texts(currentAddress));
+        $$("tbody tr").filterBy(text("State and City")).shouldHave(texts(state+city));
+        $("button#closeLargeModal").click();
+        $("#example-modal-sizes-title-lg").shouldBe(hidden);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
